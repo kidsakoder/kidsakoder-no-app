70ec0b5246fa18c1f21133022c61d1825370bd43
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-03-07 14:51:56 +0100

    Make is possible to list data from content types in the event react app

--- code/src/main/resources/assets/js/apps/events.js
+++ code/src/main/resources/assets/js/apps/events.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Graphql from './utils/graphql';
 
 class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            events: [],
+        };
+
+        this.client = new Graphql();
+        this.client.query(`
+        {
+          guillotine {
+            query(contentTypes: "no.kidsakoder.app:event") {
+              displayName
+            }
+          }
+        }
+        `, data => {
+            console.log(data)
+            this.setState(
+                Object.assign({}, this.state, {
+                    events: data.data.guillotine.query,
+                })
+            );
+        });
+    }
+
     render() {
-        return <p>Events</p>;
+        let events = this.state.events.map((e, i) => {
+            return <div key={i}>{e.displayName}</div>;
+        })
+
+        return (
+            <React.Fragment>
+                <h3>Events</h3>
+                {events}
+            </React.Fragment>
+        );
     }
 }
 
