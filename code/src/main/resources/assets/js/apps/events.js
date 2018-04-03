ceff485b73aaa2f31766f7364cede4ecb9e5b1a1
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-04-03 14:09:37 +0200

    Make it possible to collaborate between parts through portals in DOM

--- code/src/main/resources/assets/js/apps/events.js
+++ code/src/main/resources/assets/js/apps/events.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render, createPortal } from 'react-dom';
 import Graphql from './utils/graphql';
 
-class App extends React.Component {
+class Events extends React.Component {
     constructor() {
         super();
 
         this.state = {
             events: [],
+            selectedEvent: null,
         };
 
         this.client = new Graphql();
@@ -21,18 +22,43 @@ class App extends React.Component {
         });
     }
 
+    click(evt) {
+        this.setState(Object.assign({}, this.state, {
+            selectedEvent: <p>{evt.target.innerHTML}</p>,
+        }))
+    }
+
     render() {
         let events = this.state.events.map((e, i) => {
-            return <div key={i}>{e.displayName}</div>;
+            return <div key={i} onClick={this.click.bind(this)}>{e.displayName}</div>;
         });
 
         return (
             <React.Fragment>
                 <h3>Events</h3>
                 {events}
+                <Map>
+                    <h2>Dette er et kart i en portal</h2>
+                    {this.state.selectedEvent}
+                </Map>
             </React.Fragment>
         );
     }
 }
 
-render(<App/>, document.currentScript.parentElement.querySelector('.events'));
+class Map extends React.Component {
+    render() {
+        const mapElement = document.querySelector('.map');
+
+        if (mapElement) {
+            return createPortal(
+                this.props.children,
+                mapElement,
+            );
+        }
+
+        return null;
+    }
+}
+
+render(<Events/>, document.currentScript.parentElement.querySelector('.events'));
---
+++ code/src/main/resources/site/parts/map/map.es6
@@ -0,0 +1,14 @@
+var libs = {
+  portal: require('/lib/xp/portal'),
+  thymeleaf: require('/lib/xp/thymeleaf')
+};
+
+exports.get = function (request) {
+  var component = libs.portal.getComponent();
+  var content = libs.portal.getContent();
+  var view = resolve('map.html');
+
+  return {
+    body: libs.thymeleaf.render(view, { request, content, component })
+  };
+};
