fd8312b5a81c0bfae57336461f8ac531e78b1a50
Niklas Molnes Hole <nikasmh@hotmail.com>
2018-05-15 12:15:54 +0200

    Use a replace function instead of split to solve issue #92

--- code/src/main/resources/assets/js/apps/components/EventList.js
+++ code/src/main/resources/assets/js/apps/components/EventList.js
@@ -188,7 +188,7 @@ export default class EventList extends React.Component {
       return Object.assign({}, dataParsed, {
         displayName: e.displayName,
         id: e._id,
-        path: e._path.split('kidsakoder')[1],
+        path: e._path.replace(/^\/kidsakoder/, ''),
         locationParsed: this.parseCoordinates(dataParsed.location),
         published,
         date: (e.publish || {}).from || null,
